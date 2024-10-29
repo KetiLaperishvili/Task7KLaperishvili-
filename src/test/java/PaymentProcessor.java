8562a0af523ecd78b27551f777723ea9d653f317
Ketevan Laperishvili <klaperishvili@credo.ge>
2024-10-29 11:30:09 +0400

    PaymentService

---
+++ src/test/java/PaymentProcessor.java
@@ -0,0 +1,35 @@
+
+interface PaymentProcessor {
+    void process(double amount);
+}
+
+
+class CreditCardProcessor implements PaymentProcessor {
+    @Override
+    public void process(double amount) {
+        System.out.println("Processing credit card payment of $" + amount);
+    }
+}
+
+
+class PayPalProcessor implements PaymentProcessor {
+    @Override
+    public void process(double amount) {
+        System.out.println("Processing PayPal payment of $" + amount);
+    }
+}
+
+
+public class PaymentService {
+    public static void executePayment(PaymentProcessor paymentProcessor, double amount) {
+        paymentProcessor.process(amount);
+    }
+
+    public static void main(String[] args) {
+        PaymentProcessor creditCard = new CreditCardProcessor();
+        PaymentProcessor payPal = new PayPalProcessor();
+
+        executePayment(creditCard, 100.50);
+        executePayment(payPal, 75.25);
+    }
+}
